dd5ef7052489aba6c2d089344fa14171264ef018
donghyun3996 <ehdgus3996@naver.com>
2022-04-28 17:17:34 +0900

    Initial Commit

---
+++ OASIS/OASISApp.swift
@@ -0,0 +1,17 @@
+//
+//  OASISApp.swift
+//  OASIS
+//
+//  Created by 동현 on 2022/04/28.
+//
+
+import SwiftUI
+
+@main
+struct OASISApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
