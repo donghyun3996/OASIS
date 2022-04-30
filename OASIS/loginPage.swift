732635a96f9529ad58c8b13abd398c5457e29be8
donghyun3996 <ehdgus3996@naver.com>
2022-04-30 19:02:41 +0900

    button

--- OASIS/loginPage.swift
+++ OASIS/loginPage.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct loginPage: View{
+    
+    @State var idtextField = ""
+    
     var body: some View
     {
         VStack{
@@ -15,8 +18,58 @@ struct loginPage: View{
                 .frame(width: 120, height: 120)
                 .aspectRatio(contentMode: .fit)
                 .clipped()
-            Text("Login")
             
+            
+            Text("LOGIN")
+                .font(.system(size: 30))
+                .foregroundColor(Color(uiColor: UIColor.systemGray))
+                .fontWeight(.bold)
+                .padding(.top, 43)
+                .padding(.bottom, 1)
+                
+            
+            Text("앱을 사용하기위해선 로그인이 필요합니다.")
+                .font(.system(size: 11))
+                .foregroundColor(Color(uiColor:UIColor(red: 0.583, green: 0.583, blue: 0.583, alpha: 1)))
+            
+            VStack(alignment: .trailing){
+                
+            TextField("       아이디를 입력하세요", text: $idtextField)
+                .frame(width: 289, height: 51)
+                .background(Color.white)
+                .cornerRadius(14)
+                .shadow(color: Color(uiColor: UIColor(red: 0, green: 0, blue: 0, alpha: 0.08)), radius: 13, x: 0, y: 0)
+                .padding(.top, 35)
+                .padding(.bottom, 10)
+
+                
+            
+                
+            TextField("       비밀번호를 입력하세요", text: $idtextField)
+                .frame(width: 289, height: 51)
+                .background(Color.white)
+                .cornerRadius(14)
+                .shadow(color: Color(uiColor: UIColor(red: 0, green: 0, blue: 0, alpha: 0.08)), radius: 13, x: 0, y: 0)
+                
+            
+                Text("비밀번호 또는 아이디를 잊어버리셨나요?")
+                .font(.system(size: 11))
+                .foregroundColor(Color(uiColor: UIColor(red: 0.583, green: 0.583, blue: 0.583, alpha: 1)))
+            }
+                
+            Text("Login")
+                .font(.system(size: 13))
+                .fontWeight(.bold)
+                .foregroundColor(Color.white)
+                .frame(width: 289, height: 51)
+                .background(Color.blue)
+                .cornerRadius(27)
+                
+                
+                .padding(.top, 58)
+                
+    
+                
                 
                 
         }
